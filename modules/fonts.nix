10daa027beb8b2f0190bccbcd866c387cdbf54d4
uhujrouv <uhujrouv@outlook.com>
2026-03-04 17:50:09 +0800

    add cjk font

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,10 +1,16 @@
 { config, pkgs, ...}:
 {
-  home.packages = [pkgs.monaspace];
+  home.packages = with pkgs; [
+    monaspace
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+  ];
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
       monospace = ["Monaspace Neon NF"];
+      sansSerif = [ "Noto Sans CJK SC" ];
+      serif     = [ "Noto Serif CJK SC" ];
     };
   };
 }
